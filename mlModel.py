2d89342cbbe1d6e8a390fa5e93269b905340bb81
Devansh Sharma <devansh.sharma110@gmail.com>
2021-06-21 12:02:30 +0530

    password change

--- mlModel.py
+++ mlModel.py
@@ -2,8 +2,8 @@ mean_score=1
 def modelFile(LIST):
 
     import os
-    os.environ["EAI_USERNAME"] = "devansh.sharma110@gmail.com"
-    os.environ["EAI_PASSWORD"] = 'Devansh170501!'
+    os.environ["EAI_USERNAME"] = "dummysample.110@gmail.com"
+    os.environ["EAI_PASSWORD"] = '1234509876qwertpoiuyAA!'
 
 
     from expertai.nlapi.cloud.client import ExpertAiClient
@@ -25,8 +25,8 @@ def modelFile(LIST):
 
 def impTopics(LIST):
     import os
-    os.environ["EAI_USERNAME"] = "devansh.sharma110@gmail.com"
-    os.environ["EAI_PASSWORD"] = 'Devansh170501!'
+    os.environ["EAI_USERNAME"] = "dummysample.110@gmail.com"
+    os.environ["EAI_PASSWORD"] = '1234509876qwertpoiuyAA!'
 
     processData= LIST 
     print("Process data: ", *processData)
